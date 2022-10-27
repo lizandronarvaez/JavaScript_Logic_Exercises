aa4330355bd07a37817057f5cd0c24c26172e917
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2022-10-27 23:30:50 +0200

    Exercise 42

---
+++ Ejercicio_42.js
@@ -0,0 +1,16 @@
+// 42.  Leer dos números enteros y determinar si la diferencia entre los dos es un número par.
+
+function resultadoEsPrimo(numero1, numero2) {
+    // Aqui realizamos las resta de los numeros
+    let resultado = numero1 - numero2
+    // En caso de que la resta sea negativa o menor que 0, aplicamos un condicional que convierta el numero a positivo
+    resultado < 0 ? resultado = resultado * (-1) : null
+    // En caso se que el resultado sea 0
+    resultado == 0 ? console.log(`${resultado} no es un numero para evaluar`)
+        : resultado % 2 == 0 ? console.log(`La resta entre ${numero1} - ${numero2} es: ${resultado} un numero par`)
+            : console.log(`${resultado} es un numero impar`)
+    // Aqui comprobamos si la el resultado de la resta es un numero primo
+        
+}
+
+resultadoEsPrimo(10000, 10000)
